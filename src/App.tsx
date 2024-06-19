687a9f97ce0fec23a44ba0819ed9f9de61c71110
Jorge-Luis-SG <jorge.jlpm987@gmail.com>
2024-06-19 03:45:39 -0400

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
+import Beneficios from "./components/Beneficios";
 import Header from "./components/Header";
 
-import Home from "./components/Home";
+import Respaldado from "./components/Respaldado";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Respaldado/>
+      <Beneficios/>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,69 @@
+import { FiMenu } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
+
 import logo from "../assets/logo.svg";
+import { useState } from "react";
+
+export const NavMenu = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-start h-20 mb-4 lg:hidden">
+            <img src={logo} alt="Bober" />
+        </div>
+      <ul className="flex flex-col lg:flex-row justify-between items-center lg:space-x-12 text-center text-xl lg:text-lg space-y-4 lg:space-0 lg:space-y-0">
+        <li>Home</li>
+        <li>Service</li>
+        <li>Feature</li>
+        <li>Product</li>
+        <li>Testimonial</li>
+        <li>FAQ</li>
+      </ul>
+      <div className="login flex lg:hidden mt-8 space-x-4">
+        <button className="bg-transparent text-[#FFA600] w-24 h-11 text-lg">
+          Login
+        </button>
+        <button className="bg-[#FFA600] text-white py-2 w-24 h-11 text-lg">
+          Sign up
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default function Header() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
   return (
     <>
-      <header className="flex py-5 bg-[#F5F7FA] pr-20 pl-4">
-        <div className="flex justify-start">
-          <img src={logo} alt="Bober" />
-        </div>
-        <nav className="max-w-xl mx-auto flex items-center ">
-          <ul className="flex justify-between items-center space-x-12">
-            <li>Home</li>
-            <li>Service</li>
-            <li>Feature</li>
-            <li>Product</li>
-            <li>Testimonial</li>
-            <li>FAQ</li>
-          </ul>
-        </nav>
-        <div className="login flex">
-          <button className="bg-transparent text-[#FFA600] mr- w-20">
-            Login
-          </button>
-          <button className="bg-[#FFA600] text-white w-20">Sign up</button>
+      <header className="py-5 bg-[#F5F7FA] pr-20 pl-4">
+        <div className={`fixed lg:relative flex justify-between max-w-[1700px] mx-auto w-full left-0 top-0 ${menuIsOpen ? 'bg-[#ffffff90]' : 'bg-transparent'} lg:bg-transparent`}>
+          <div className={`flex items-center justify-start h-20 ${menuIsOpen ? 'opacity-0' : 'opacity-100'} lg:opacity-100`}>
+            <img src={logo} alt="Bober" />
+          </div>
+          <nav className={`absolute left-auto right-0 z-50 sm:relative max-w-xl lg:mx-auto flex flex-col items-center justify-center  h-screen  lg:w-auto lg:h-auto bg-[#efe7e7] lg:bg-transparent ${menuIsOpen ? 'w-full' : 'w-auto'} transition-all `}>
+            <div className="absolute top-4 right-4 lg:hidden w-12 h-12">
+              {menuIsOpen ? (
+                <MdClose
+                  className="w-12 h-12"
+                  onClick={() => setMenuIsOpen(false)}
+                />
+              ) : (
+                <FiMenu
+                  className="w-12 h-12"
+                  onClick={() => setMenuIsOpen(true)}
+                />
+              )}
+            </div>
+            <div className="hidden lg:block">
+              <NavMenu />
+            </div>
+            <div className="lg:hidden">{menuIsOpen ? <NavMenu /> : <></>}</div>
+          </nav>
+          <div className="login hidden lg:flex lg:items-center">
+            <button className="bg-transparent text-[#FFA600] w-24 h-11">
+              Login
+            </button>
+            <button className="bg-[#FFA600] text-white w-24 h-11">Sign up</button>
+          </div>
         </div>
       </header>
     </>
