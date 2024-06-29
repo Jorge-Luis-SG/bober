d4d42fd88ebf0fcb539a2a0d00b7b8bf5b87ac7c
Jorge Pérez <Jorge.jlpm987@gmail.com>
2024-06-29 16:17:23 -0400

    Update Newsletter.tsx

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -17,15 +17,29 @@ export function Newsletter() {
        }) 
     }
 
+    const handleSubmit = async () => {
+
+        const response = await fetch('http://localhost:3000/api/send-email', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData),
+        })
+
+        const result = await response.json();
+        console.log(result)
+    }
+
   return (
     <section className="bg-[#F5F7FA]">
         <div className='flex flex-col px-32 py-14 max-w-6xl m-auto sm:space-y-0 space-y-4 items-center'>
             <h2 className='text-[#001441] text-4xl font-semibold text-center pb-4'>Suscríbete a nuestro boletín para estar al tanto de <br />todo con Bober.</h2>        
-            <div className='flex flex-col items-center justify-center space-y-4'>
+            <form onSubmit={(e) => {e.preventDefault();handleSubmit()}} className='flex flex-col items-center justify-center space-y-4'>
                 <input onChange={(e) => {handleChange(e)}} value={formData.name} className='block bg-black/20 text-black placeholder-black py-2 px-2 rounded-lg' type="text" name="name" placeholder="Nombre" />
                 <input onChange={(e) => {handleChange(e)}} value={formData.email} className='block bg-black/20 text-black placeholder-black py-2 px-2 rounded-lg' type="text" name="email" placeholder="Correo electrónico" />
-                <button className='flex items-center text-white bg-[#FFA600] px-8 py-2 rounded space-x-2'>Enviar <img className='pl-1' src={ArrowRight} alt="" /></button>
-            </div>
+                <button type='submit' className='flex items-center text-white bg-[#FFA600] px-8 py-2 rounded space-x-2'>Enviar <img className='pl-1' src={ArrowRight} alt="" /></button>
+            </form>
         </div>
     </section>
   )
